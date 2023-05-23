d076665ed08ba324be9d3e9f8e533631f5bbfc8e
James Otwane <127046977+ja259@users.noreply.github.com>
2023-05-23 18:06:34 -0400

    Add javascript.js

--- javascript.js
+++ javascript.js
@@ -2,6 +2,12 @@
 
 document.addEventListener("DOMContentLoaded", function(){
      createBoard(16);
+
+     let btn_popup = document.querySelector("#popup");
+     btn_popup.addEventListener("click", function(){
+        let size = getSize();
+        createBoard(size);
+})
 })
 
 function createBoard(size){
@@ -14,6 +20,21 @@ let numDivs = size * size;
 
 for(let i = 0; i < numDivs; i++){
     let div = document.createElement("div");
-    div.style.backgroundColor = "blue";
+    div.addEventListener("mouseover", colorDiv);
     board.insertAdjacentElement("beforeend", div);
+}
+
+function getSize(){
+    let input = prompt("What will be the size of the board?");
+    let message = document.querySelector("#message");
+    if(input == ""){
+        message.innerHTML = "Please provide a number";
+    }
+    else if(input < 0 || input > 100){
+        message.innerHTML = "Provide a number between 1 and 100";
+    }
+    else{
+        message.innerHTML = "Now you can play!";
+        return input;
+    }
 }
