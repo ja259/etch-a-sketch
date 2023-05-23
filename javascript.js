28ced4f84afd897d2b4d999ccfbb920aac5ff86a
James Otwane <127046977+ja259@users.noreply.github.com>
2023-05-23 17:32:07 -0400

    Add javascript.js

--- javascript.js
+++ javascript.js
@@ -9,3 +9,11 @@ function createBoard(size){
     board.getElementsByClassName.grigTemplateColumns = `repeat(${size},1fr)`;
     board.getElementsByClassName.grigTemplateRows = `repeat(${size},1fr)`;
 }
+
+let numDivs = size * size;
+
+for(let i = 0; i < numDivs; i++){
+    let div = document.createElement("div");
+    div.style.backgroundColor = "blue";
+    board.insertAdjacentElement("beforeend", div);
+}
