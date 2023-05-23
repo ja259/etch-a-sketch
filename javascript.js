af6c67ecc77a3c060c41111e632f7d6c6527e623
James Otwane <127046977+ja259@users.noreply.github.com>
2023-05-22 21:40:05 -0400

    Add javascript.js

---
+++ javascript.js
@@ -0,0 +1,7 @@
+/* javascript.js */
+
+document.addEventListener("DOMContentLoaded", function(){
+    let board = document.querySelector(".board");
+    board.getElementsByClassName.grigTemplateColumns = "repeat(16,1fr)";
+    board.getElementsByClassName.grigTemplateRows = "repeat(16,1fr)";
+})
